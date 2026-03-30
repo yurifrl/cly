d4827d4912688dc49bb7985a97d52debd2c11c24
Yuri <3260068+yurifrl@users.noreply.github.com>
2026-03-30 00:51:02 -0300

    feat: add print-key module

---
+++ modules/demo/print-key/printkey.go
@@ -0,0 +1,49 @@
+package printkey
+
+import (
+	tea "charm.land/bubbletea/v2"
+	"github.com/spf13/cobra"
+)
+
+type model struct{}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyboardEnhancementsMsg:
+		return m, tea.Printf("Keyboard enhancements: EventTypes: %v\n",
+			msg.SupportsEventTypes())
+	case tea.KeyMsg:
+		key := msg.Key()
+		switch msg := msg.(type) {
+		case tea.KeyPressMsg:
+			switch msg.String() {
+			case "ctrl+c":
+				return m, tea.Quit
+			}
+		}
+		format := "(%T) You pressed: %s"
+		args := []any{msg, msg.String()}
+		if len(key.Text) > 0 {
+			format += " (text: %q)"
+			args = append(args, key.Text)
+		}
+		return m, tea.Printf(format, args...)
+	}
+	return m, nil
+}
+
+func (m model) View() tea.View {
+	v := tea.NewView("Press any key to see its details printed to the terminal. Press 'ctrl+c' to quit.")
+	v.KeyboardEnhancements.ReportEventTypes = true
+	return v
+}
+
+func run(cmd *cobra.Command, args []string) error {
+	p := tea.NewProgram(model{})
+	_, err := p.Run()
+	return err
+}
